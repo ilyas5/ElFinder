7633b04ac1a085ea2032dd57aa0735203b042e95
Muhammad Ilyas <ilyas.pesh@gmail.com>
2020-12-02 12:08:55 +0500

    file upload in Elfinder

---
+++ ElFinder/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ElFinder
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Content/elfinder/js/jquery.js", "~/Content/elfinder/js/jquery-ui.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/elfinder").Include(
+                       "~/Content/elfinder/js/elfinder.min.js"));
+        }
+    }
+}
---
+++ ElFinder/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using ElFinder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElFinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string subFolder)
+        {
+            FileViewModel model = new FileViewModel { Folder = "Files", SubFolder = subFolder };
+            return View(model);
+        }
+
+       
+    }
+}
